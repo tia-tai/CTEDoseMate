6323f3c5c1abfd52df122f448b9b69dc876666aa
Tai Yu Lin <155038045+tia-tai@users.noreply.github.com>
2025-10-20 21:34:05 -0400

    Created servo functions and a time checker
    Modified values array to include an additional parameter. Adjusted logic for reading button input and handling data from Serial2.

--- esp.c
+++ esp.c
@@ -3,13 +3,17 @@
 #include <WiFiUdp.h>
 #include <ESP32Servo.h>
 
-const char *ssid     = "GOVERNMENTSPY";
-const char *password = "mrtakeyobi";
+const char *ssid     = "Chen";
+const char *password = "ChenYanYun";
 
 Servo sizeSelector;
 Servo slotSelector;
 
-int values[6][3]  = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+int buttonVal;
+const int slotDelay = 900;
+const int sizeDelay = 800;
+
+int values[6][4]  = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
 String datam;
 int selectum = 0;
 int hourTo;
@@ -21,9 +25,20 @@ int lastMinute = -1;
 
 NTPClient timeClient(ntpUDP, "us.pool.ntp.org", -14400, 3600);
 
+
 void setup(){
   sizeSelector.attach(12);
   slotSelector.attach(13);
+  pinMode(14, OUTPUT);
+  pinMode(15, INPUT_PULLUP);
+  buttonVal = digitalRead(15);
+
+  sizeSelector.write(90);
+
+  while (buttonVal != LOW){
+    slotSelector.write(0);
+  }
+  slotSelector.write(90);
 
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, 16, 17);
@@ -38,6 +53,7 @@ void setup(){
 
 void loop() {
   timeClient.update();
+  buttonVal = digitalRead(15);
 
   int currentHour = timeClient.getHours();
   int currentMinute = timeClient.getMinutes();
@@ -51,16 +67,12 @@ void loop() {
   }
 
   if (Serial2.available()) {
-    Serial.println("Hi");
     datam = Serial2.readStringUntil('\n');
-    Serial.println(datam);
 
     int spaceIndex = datam.indexOf(' ');
 
     String firstWord = datam.substring(0, spaceIndex);
     String slotNumber = datam.substring(spaceIndex + 1);
-    Serial.println(firstWord);
-    Serial.println(slotNumber);
    
     if (firstWord == "Slot:") {
       selectum = slotNumber.toInt();
@@ -69,8 +81,9 @@ void loop() {
     } else if (firstWord == "Interval:") {
       hourTo = (currentHour + slotNumber.toInt()) % 24;
       values[selectum][1] = (hourTo*60)+currentMinute;
-    } else if (firstWord == "Amount:") {
       values[selectum][2] = slotNumber.toInt();
+    } else if (firstWord == "Amount:") {
+      values[selectum][3] = slotNumber.toInt();
     }
   }
 
@@ -78,17 +91,37 @@ void loop() {
     int pillsize;
     int interval;
     int amount;
-    for (int x = 0; x <= 2; x++) {
+    for (int x = 0; x <= 3; x++) {
       if(x==0) {
         pillsize = values[i][x];
       } else if(x==1) {
         interval = values[i][x];
-      } else if(x==2) {
+      } else if(x==3) {
         amount = values[i][x];
       }
     }
-    if (interval == (currentHour*60)+currentMinute) {
-      //Do servo thing
+    if (interval <= (currentHour*60)+currentMinute) {
+      slotSelector.write(180);
+      delay(slotDelay*i);
+      slotSelector.write(90);
+      for (int z = 0; z <= amount-1; i++) {
+        sizeSelector.write(180);
+        delay(sizeDelay*pillsize);
+        sizeSelector.write(90);
+        delay(1000);
+        sizeSelector.write(0);
+        delay(1600); // figure out the numbers tmr
+      }
+
+      tone(14, 1000);
+      delay(1000);
+      noTone(14);
+      hourTo = (currentHour + values[selectum][2]) % 24;
+      values[selectum][1] = (hourTo*60)+currentMinute;
+      while (buttonVal != LOW){
+        slotSelector.write(0);
+      }
+      slotSelector.write(90);
     }
   }
 
