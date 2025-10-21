a26e8e04b654e129b0caf2594c570181a63c02a1
Tai Yu Lin <155038045+tia-tai@users.noreply.github.com>
2025-10-20 21:30:38 -0400

    Update WiFi credentials and refactor servo handling
    Updated WiFi credentials and replaced Servo library with ESP32Servo. Modified data handling and parsing logic.

--- esp.c
+++ esp.c
@@ -1,18 +1,19 @@
 #include <NTPClient.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
-#include <Servo.h>
+#include <ESP32Servo.h>
+#include <string.h>
 
-const char *ssid     = "Chen";
-const char *password = "ChenYanYun";
+const char *ssid     = "Galaxy S24 Ultra 2CA9";
+const char *password = "mrtakeyobi";
 
 Servo sizeSelector;
 Servo slotSelector;
 
 int values[6][3]  = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-String data;
-int menum = 0;
+String datam;
 int selectum = 0;
+int hourTo;
 
 WiFiUDP ntpUDP;
 
@@ -41,17 +42,35 @@ void loop() {
 
   int currentHour = timeClient.getHours();
   int currentMinute = timeClient.getMinutes();
-  
+ 
   if (currentMinute != lastMinute) {
     lastMinute = currentMinute;
-    
+   
     formattedTime = (currentHour < 10 ? "0" : "") + String(currentHour) + ":" +
                     (currentMinute < 10 ? "0" : "") + String(currentMinute);
+    Serial.println(formattedTime);
   }
 
   if (Serial.available() > 0) {
-    data = Serial2.readStringUntil('\n');
-    if ("Slot" )
+    datam = Serial2.readStringUntil('\n');
+
+    char charBuffer[datam.length() + 1];
+    datam.toCharArray(charBuffer, sizeof(charBuffer));
+
+    char* token = strtok(charBuffer, " ");
+
+   
+    if (String(token) == "Slot:") {
+      selectum = (int)strtok(NULL, " ");
+    } else if (String(token) == "Size:") {
+      values[selectum][0] = (int)strtok(NULL, " ");
+    } else if (String(token) == "Interval:") {
+      hourTo = (currentHour + (int)strtok(NULL, " ")) % 24;
+      values[selectum][1] = (String)hourTo + ":" + (String)currentMinute;
+      Serial.println(value[selectum][1]);
+    } else if (String(token) == "Amount:") {
+      values[selectum][2] = (int)strtok(NULL, " ");
+    }
   }
 
   delay(1000);
